4358fbe4077c9d7a14541dee6fe93dea9d53252d
=kobakhit <=kobakhit@gmail.com>
2020-12-24 19:55:31 -0500

    theme

---
+++ themes/hello-friend/assets/js/user.js
@@ -0,0 +1,11 @@
+// MDB Lightbox Init
+$(function () {
+    $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+    });
+  
+  alert('hello')
+  
+  // A $( document ).ready() block.
+  $( document ).ready(function() {
+    console.log( "ready!" );
+  });
